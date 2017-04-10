0f927d358eee8be58d7915904a448f5ec01a47f0
Jan Mozgawa <jmozgawa@gmail.com>
2017-04-10 18:25:17 +0200

    add ability login with facebook

--- server/src/facebook.js
+++ server/src/facebook.js
@@ -1,4 +1,5 @@
 const facebookIds = {
+    appToken: '794881630542767|msmPIsdcyrdakWXiuWMwMXUtPGQ',
     clientID: '794881630542767',
     clientSecret: '3f894e82fb24b6da19bc7a85467f33e5',
     callbackURL: 'http://127.0.0.1:8080/auth/facebook/callback'
