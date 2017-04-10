2cc7597a7a3b34acdb974797e0fe81159a3acf30
Lukasz Gandecki <lgandecki@css.edu>
2017-04-10 16:15:00 +0200

    initial try for facebook login on the server and client side

---
+++ server/src/facebook.js
@@ -0,0 +1,7 @@
+const facebookIds = {
+    clientID: '794881630542767',
+    clientSecret: '3f894e82fb24b6da19bc7a85467f33e5',
+    callbackURL: 'http://127.0.0.1:8080/auth/facebook/callback'
+}
+
+export default facebookIds;
